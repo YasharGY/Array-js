ad064972e93d9440f639f2f15148ac5205051fa8
YasharGY <yasharagu@code.edu.az>
2023-05-05 03:56:20 +0400

    Array

---
+++ main.js
@@ -0,0 +1,43 @@
+//  let numbers = [ -3, 8, 7, 6, 5, -4, 3, 2, 1 ];
+//  numbers.sort((a,b)=>a - b);
+//  console.log(numbers)
+
+// const myArr = [[1, 2, 3], [100, 2, 1, 10]];
+// const newArr = myArr.flatMap((x, i) =>{
+//     return x
+// })
+// console.log(newArr)
+
+// // function removeValues(arr){
+// //     return arr.filter(function(value) {
+// //         return Boolean(value);
+// //     })
+// // }
+// // var array = [NaN, 0, 15, false, -22,"", undefined, 47, null]
+// // var filteredArray = removeValues(array)
+// // console.log(filteredArray);
+
+// function sortByTitle(arr) {
+//     arr.sort(function(a, b) {
+//       var titleA = a.title.toLowerCase();
+//       var titleB = b.title.toLowerCase();
+//       if (titleA < titleB) {
+//         return -1;
+//       }
+//       if (titleA > titleB) {
+//         return 1;
+//       }
+//       return 0;
+//     });
+//     return arr;
+//   }
+  
+//   var library = [
+//     { author: 'Bill Gates', title: 'The Road Ahead', libraryID: 1254 },
+//     { author: 'Steve Jobs', title: 'Walter Isaacson', libraryID: 4264 },
+//     { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', libraryID: 3245 }
+//   ];
+  
+//   var sortedLibrary = sortByTitle(library);
+//   console.log(sortedLibrary);
+  
